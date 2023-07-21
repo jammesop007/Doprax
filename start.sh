ff3bc7b08e17caf6c63e876fe25ed4f23b84dd63
jammesop007 <140200566+jammesop007@users.noreply.github.com>
2023-07-22 00:34:27 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone $REPO_URL ok && cd ok && pip3 install -U -r requirements.txt
-$START_CMD
+git clone https://github.com/jammesop007/WZML-X ok && cd ok && pip3 install -U -r requirements.txt
+python3 -m bot
